24b77c9bae2e5f2b873286ecb164fbf2994bd929
yuya <kinjo.bobchin@gmail.com>
2023-09-25 13:54:51 +0900

    test

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-console.log("Hello via Bun!");
+import { execSync } from "child_process";
+
+// const command = "git ls-files --cached";
+const command = "git diff --name-only";
+const diffFiles = execSync(command).toString();
+const toArray = diffFiles.split("\n").filter((file) => file !== "");
+
+console.log("🚀 ~ diffFiles:", diffFiles);
+console.log(toArray);
